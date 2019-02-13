a0bab71b6d32caa62b0e2e505f9116cc4779d404
jffornieles <jffornieles@gmail.com>
2019-02-13 11:54:10 +0100

    Eliminación de espacios

--- Westeros/Controllers/HouseDetailViewController.swift
+++ Westeros/Controllers/HouseDetailViewController.swift
@@ -41,6 +41,7 @@ class HouseDetailViewController: UIViewController {
         houseNameLabel.text = "House \(model.name)"
         sigilImageView.image = model.sigil.image
         wordsLabel.text = model.words
+        title = model.name
     }
     
     // MARK: - UI
